c89b897f04b882a7d7021a5c4b5161fd37838a2f
Sanchika123 <79789552+Sanchika123@users.noreply.github.com>
2021-09-28 19:42:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+img="";
+Status="";
+objects="";
+function preload(){
+    img=loadImage('dog_cat.jpg');
+}
+
+function setup(){
+    canvas=createCanvas(640, 420);
+    canvas.center();
+    objectDetector= ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById('status').innerHTML= 'status: detecting objects';
+}
+function modelLoaded(){
+    console.log('Model Loaded!');
+    Status= true;
+    objectDetector.detect(img,gotresult);
+}
+function gotresult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects= results;
+    }
+}
+function draw(){
+    image(img, 0, 0, 640, 420);
+    if(Status != ""){
+        for(i=0; i<objects.length; i++){
+            document.getElementById('status').innerHTML= 'status: objects detected';
+            percent= floor(objects[i].confidence*100);
+            fill('#FF0000');
+            text(objects[i].label +" "+percent+"%", objects[i].x+15, objects[i].y+15);
+            noFill();
+            stroke('#FF0000');
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
